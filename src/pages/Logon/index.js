8a20f084807b9041d210d7cbf8141c1ef180ba30
Max Zanelato <maxzanelato@gmail.com>
2020-03-25 19:13:28 -0300

    Página de registro pronta

--- src/pages/Logon/index.js
+++ src/pages/Logon/index.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 import { FiLogIn } from "react-icons/fi";
 
@@ -21,10 +22,10 @@ export default function Logon() {
             Entrar
           </button>
 
-          <a href="/register">
+          <Link to="/register" className="back-link">
             <FiLogIn size={16} color="#E02041" />
             Não tenho cadastro
-          </a>
+          </Link>
         </form>
       </section>
       <img src={heroesImg} alt="Heroes" />
