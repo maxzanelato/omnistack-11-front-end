49d7ac8cc171201c0612099d61d531ebfac09f68
Max Zanelato <maxzanelato@gmail.com>
2020-03-26 13:25:19 -0300

    Deleção e lista de casos prontos.

--- src/pages/Profile/index.js
+++ src/pages/Profile/index.js
@@ -1,5 +1,7 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
+
+import api from "../../services/api";
 
 import "./styles.css";
 
@@ -7,7 +9,38 @@ import logoImg from "../../assets/logo.svg";
 import { FiPower, FiTrash2 } from "react-icons/fi";
 
 export default function Profile() {
+  const [incidents, setIncidents] = useState([]);
+
   const ongName = localStorage.getItem("ongName");
+  const ongId = localStorage.getItem("ongId");
+
+  /* Dispara evento para carregar em tela */
+  useEffect(() => {
+    api
+      .get("profiles", {
+        headers: {
+          Authorization: ongId
+        }
+      })
+      .then(response => {
+        setIncidents(response.data);
+      });
+  }, [ongId]);
+
+  async function handleDeleteIncident(id) {
+    try {
+      await api.delete(`incidents/${id}`, {
+        headers: {
+          Authorization: ongId
+        }
+      });
+
+      setIncidents(incidents.filter(incident => incident.id !== id));
+    } catch (error) {
+      alert("Erro ao deletar caso! Tente novamente.");
+    }
+  }
+
   return (
     <div className="profile-container">
       <header>
@@ -23,65 +56,31 @@ export default function Profile() {
       </header>
       <h1>Casos cadastrados</h1>
       <ul>
-        <li>
-          <strong>Caso:</strong>
-          <p>Caso teste</p>
+        {incidents.map(incident => (
+          <li key={incident.id}>
+            <strong>Caso:</strong>
+            <p>{incident.title}</p>
 
-          <strong>DESCRIÇÃO:</strong>
-          <p>Descrição do teste</p>
+            <strong>DESCRIÇÃO:</strong>
+            <p>{incident.description}</p>
 
-          <strong>Valor:</strong>
-          <p>R$ 120,00</p>
+            <strong>Valor:</strong>
+            <p>
+              {Intl.NumberFormat("pt-BR", {
+                style: "currency",
+                currency: "BRL"
+              }).format(incident.value)}
+            </p>
 
-          <button type="button">
-            <FiTrash2 size={20} color="#a8a8b3" />
-          </button>
-        </li>
-
-        <li>
-          <strong>Caso:</strong>
-          <p>Caso teste</p>
-
-          <strong>DESCRIÇÃO:</strong>
-          <p>Descrição do teste</p>
-
-          <strong>Valor:</strong>
-          <p>R$ 120,00</p>
-
-          <button type="button">
-            <FiTrash2 size={20} color="#a8a8b3" />
-          </button>
-        </li>
-
-        <li>
-          <strong>Caso:</strong>
-          <p>Caso teste</p>
-
-          <strong>DESCRIÇÃO:</strong>
-          <p>Descrição do teste</p>
-
-          <strong>Valor:</strong>
-          <p>R$ 120,00</p>
-
-          <button type="button">
-            <FiTrash2 size={20} color="#a8a8b3" />
-          </button>
-        </li>
-
-        <li>
-          <strong>Caso:</strong>
-          <p>Caso teste</p>
-
-          <strong>DESCRIÇÃO:</strong>
-          <p>Descrição do teste</p>
-
-          <strong>Valor:</strong>
-          <p>R$ 120,00</p>
-
-          <button type="button">
-            <FiTrash2 size={20} color="#a8a8b3" />
-          </button>
-        </li>
+            <button
+              type="button"
+              /* É feito dessa forma, senão, quando renderizar o botão, todos os incidentes serão deletados. É necessário passar uma função. */
+              onClick={() => handleDeleteIncident(incident.id)}
+            >
+              <FiTrash2 size={20} color="#a8a8b3" />
+            </button>
+          </li>
+        ))}
       </ul>
     </div>
   );
