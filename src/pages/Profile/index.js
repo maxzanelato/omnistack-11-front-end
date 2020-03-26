a9fd2693077c01ac846415e8cb10208c56781603
Max Zanelato <maxzanelato@gmail.com>
2020-03-26 12:49:49 -0300

    Login se conectando com o backend

--- src/pages/Profile/index.js
+++ src/pages/Profile/index.js
@@ -7,11 +7,12 @@ import logoImg from "../../assets/logo.svg";
 import { FiPower, FiTrash2 } from "react-icons/fi";
 
 export default function Profile() {
+  const ongName = localStorage.getItem("ongName");
   return (
     <div className="profile-container">
       <header>
         <img src={logoImg} alt="Logo" />
-        <span>Bem vinda, APAD</span>
+        <span>Bem vinda, {ongName}</span>
 
         <Link className="button" to="/incidents/new">
           Cadastrar novo caso
